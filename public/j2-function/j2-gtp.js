dd4fb12c238cc3222953dce369e133ea7836d587
batchmate <132881405+jhonjalany@users.noreply.github.com>
2025-07-07 23:22:34 +0800

    Delete public/j2-function directory

--- public/j2-function/j2-gtp.js
+++
@@ -1,7 +0,0 @@
-function goToPage(page) {
-  if (isEditing) disableEditing();
-  const totalPages = Math.ceil(filteredData.length / pageSize);
-  if (page < 1 || page > totalPages) return;
-  currentPage = page;
-  renderTable();
-}
