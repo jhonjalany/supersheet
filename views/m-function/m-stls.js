c41bd9835fb73187862674fed23f4fdeb818606e
batchmate <132881405+jhonjalany@users.noreply.github.com>
2025-07-15 17:35:27 +0800

    Delete views/m-function directory

--- views/m-function/m-stls.js
+++
@@ -1,8 +0,0 @@
-function saveToLocalStorage() {
-  const currentSheet = document.getElementById('sheetSelector').value;
-  localStorage.setItem('residentData', JSON.stringify(residentData));
-  localStorage.setItem('sheetDataMap', JSON.stringify(sheetDataMap));
-  localStorage.setItem('currentSheet', currentSheet);
-  localStorage.setItem('sheetFormulas', JSON.stringify(window.sheetFormulas || {}));
-  localStorage.setItem('editedCells', JSON.stringify(editedCells)); // Ensure edits are saved
-}  
